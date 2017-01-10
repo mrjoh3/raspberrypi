5c50f133de0cc4ca1fc2716eeda3d61f79abba72
Matt Johnson <mrjoh3@gmail.com>
2017-01-11 09:16:13 +1100

    create switch.py

---
+++ switch.py
@@ -0,0 +1,14 @@
+# from: http://razzpisampler.oreilly.com/ch07.html
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP) # or try 16
+
+while True:
+    input_state = GPIO.input(21)
+    if input_state == False:
+        print('Button Pressed')
+        time.sleep(0.2)
